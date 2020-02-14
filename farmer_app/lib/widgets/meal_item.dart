fa117b40aa816f1f443f0ffeee7d4e8476881c5a
AbhayVAshokan <abhayvashokan@gmail.com>
2020-02-15 01:23:58 +0530

    added launcher icon

--- farmer_app/lib/widgets/meal_item.dart
+++ farmer_app/lib/widgets/meal_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../screens/meal_detail_screen.dart';
+// import '../screens/meal_detail_screen.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
@@ -53,10 +53,10 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(
-      MealDetailScreen.routeName,
-      arguments: id,
-    );
+    // Navigator.of(context).pushNamed(
+    //   MealDetailScreen.routeName,
+    //   arguments: id,
+    // );
   }
 
   @override
